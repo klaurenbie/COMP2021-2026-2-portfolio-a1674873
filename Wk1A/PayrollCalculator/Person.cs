23a04f578bba9faba05fd0687d2e4db050168f8a
klaurenbie <a1674873@adelaide.edu.au>
2026-08-06 15:46:42 +0930

    feat: add Person class with validation

---
+++ Wk1A/PayrollCalculator/Person.cs
@@ -0,0 +1,32 @@
+namespace PayrollCalculator;
+public class Person
+{
+    // Auto-properties
+
+    public int Age { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Constructor
+    public Person(string first, string last, int age)
+    {
+        FirstName = first;
+        LastName  = last;
+        Age       = age;
+    }
+
+    // Method
+    public string FullName()
+    {
+        return $"{LastName}, {FirstName}";
+    }
+
+    public bool IsAdult()
+    {
+       if (Age < 18)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Wk1A/PayrollCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Pipes;
+
+namespace PayrollCalculator;
+
+class Program
+{
+    static double TAX_RATE = 0.2;
+    static void Main(string[] args)
+    {
+        Console.Write("Enter employee first name: ");
+        string first = Console.ReadLine();
+        
+        Console.Write("Enter employee last name: ");
+        string last = Console.ReadLine();
+
+        Console.Write("Enter employee age: ");
+        int age = int.Parse(Console.ReadLine());        
+
+        var p = new Person(first, last, age);
+        
+        Console.Write("Hours worked: ");
+        double hours = double.Parse(Console.ReadLine());
+
+        Console.Write("Hourly rate: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        double net_pay = CalculatePay(hours, rate);
+        Console.WriteLine($"{p.fist} earned ${net_pay:F2} after tax.");
+        
+    }
+
+    static double CalculatePay(double hours, double rate)
+    {
+        if (hours < 0 || rate < 0)
+        {
+            throw new ArgumentException("Hours and rate must be positive.");
+        }
+
+        double gross = hours * rate;
+        double tax = gross * TAX_RATE;
+        double net = gross - tax;
+
+        return net;
+    }
+}
+
